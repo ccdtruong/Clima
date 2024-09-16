d548c39a67dfcfa5d3b12a4045865cc8b26e3b18
ccdtruong <ccdtruong@gmail.com>
2024-09-16 21:44:27 +0700

    Create request to OpenWeather api

--- Model/APIManager.swift
+++ Model/APIManager.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct APIManager {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
+    var appid = "fa2e7641b493088d4b4a808c143b2c23"
+    var cityName = "Hanoi"
+    
+    mutating func setCityname(_ city : String) {
+        cityName = city
+    }
+    
+    func fecthWeather() {
+        
+        let urlString = createRequestUrl()
+        print(urlString)
+        performRequest(urlString)
+    }
+    
+    func createRequestUrl() -> String {
+        return "\(apiUrl)appid=\(appid)&q=\(cityName)"
+    }
+    
+    func performRequest(_ urlString : String)
+    {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let dataTask = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let responseData = data {
+                    let dataString = String(data: responseData, encoding: .utf8)
+                    print(dataString ?? "nil")
+                }
+            }
+            dataTask.resume()
+        }
+    }
+}
